8fcc0a72abf8324ba8daf63dd839baf8127f15a4
oursu-broad <oursu@broadinstitute.org>
2020-07-16 14:52:59 -0400

    docs

--- perturbseq/io.py
+++ perturbseq/io.py
@@ -16,10 +16,22 @@ def read_perturbations_csv(my_adata,
     ----
     my_adata: AnnData
         adata
+    cell2guide_csv: str
+        csv file where each line is a cell and each column in a guide. It has a 1 if the guide is present in the cell and a 0 otherwise.                                                             
+    guide2gene_csv: str
+        (optional) a csv file mapping which gene is targeted by each guide. This is useful for analyses aggregating across all guides of a gene.                                                     
+    pref: str 
+        (default: "perturb"): (optional) a prefix to add to annotations. This is meant to allow the user to have potentially multiple modes of perturbations allowed.                                           
+    sep: str
+        (default: "\\t"): (optional) separator in the csv file.                                        
+    copy: bool
+        (default: False): (optional) whether to return a copy of the annotation data                  
+              
     
     Returns
     -------
-    Adds the to adata (or copy thereof) the following fields:
+    None
+        adds the to adata (or copy thereof) the following fields:
 
             adata.obsm[pref+'.cell2guide']
 
