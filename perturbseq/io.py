f9abe0a30178523e3245d700b0102d7607fc1f8b
oursu-broad <oursu@broadinstitute.org>
2020-07-16 14:16:18 -0400

    docs

--- perturbseq/io.py
+++ perturbseq/io.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 
-def read_perturbations_csv(data,
+def read_perturbations_csv(my_adata,
                            cell2guide_csv: str,
                            guide2gene_csv: str = None,
                            pref: str = 'perturb',
@@ -14,23 +14,34 @@ def read_perturbations_csv(data,
     Read in which perturbations were present in which cell.
 
     Args:
-    ----
-    data
+    --------
+
+    my_adata
         adata
     cell2guide_csv 
         csv file where each line is a cell and each column in a guide. It has a 1 if the guide is present in the cell and a 0 otherwise.
     guide2gene_csv
         (optional) a csv file mapping which gene is targeted by each guide. This is useful for analyses aggregating across all guides of a gene.
-    pref: 
+    pref (default: "perturb")
+        (optional) a prefix to add to annotations. This is meant to allow the user to have potentially multiple modes of perturbations allowed.
+    sep (default: "\t")
+        (optional) separator in the csv file. 
+    copy (default: False)
+        (optional) whether to return a copy of the annotation data
+    
     
     Returns:
-        something: something
-    """
+    -----------
     
+    Adds the to adata (or copy thereof) the following fields:
+        adata.obsm[pref+'.cell2guide']
+        adata.obsm[pref+'.cell2gene'] 
+        
+    """
 
     import pandas as pd
 
-    if copy: adata_here = adata_here.copy()
+    if copy: my_adata = my_adata.copy()
     
     #read cell2guide
     cell2guide=pd.read_csv(cell2guide_csv,sep=sep)
@@ -38,15 +49,15 @@ def read_perturbations_csv(data,
         
     #check that the annotated cells have a good overlap with the adata
     cells_annotated=list(set(cell2guide['cell']))
-    cells_annotated_in_adata=list(set(cells_annotated).intersection(set(adata_here.obs_names)))
-    percent_cells_annotated_in_adata=100*round(len(cells_annotated_in_adata)/adata_here.n_obs,2)
-    print('adata cells:',adata_here.n_obs)
+    cells_annotated_in_adata=list(set(cells_annotated).intersection(set(my_adata.obs_names)))
+    percent_cells_annotated_in_adata=100*round(len(cells_annotated_in_adata)/my_adata.n_obs,2)
+    print('adata cells:',my_adata.n_obs)
     print('annotated cells:',len(cells_annotated),'of which',percent_cells_annotated_in_adata,'percent in adata')
     
     #assign the guides to the cells in adata
     guides=list(set(cell2guide.columns).difference(set(['cell'])))
     cell2guide_for_adata=pd.DataFrame(0.0,
-                      index=adata_here.obs_names,
+                      index=my_adata.obs_names,
                       columns=guides)
     cell2guide_for_adata.loc[cells_annotated_in_adata,guides]=cell2guide.loc[cells_annotated_in_adata,guides]
     
@@ -55,7 +66,7 @@ def read_perturbations_csv(data,
         guide2gene=pd.read_csv(guide2gene_csv,sep=sep)
         genes=list(set(guide2gene['gene']))
         cell2gene_for_adata=pd.DataFrame(0.0,
-                      index=adata_here.obs_names,
+                      index=my_adata.obs_names,
                       columns=genes)
         #for each gene, check if at least one of the guides is present
         for gene in genes:
@@ -64,9 +75,9 @@ def read_perturbations_csv(data,
             if len(guides_per_gene)>0:
                 cell2gene_for_adata.loc[cells_annotated_in_adata,gene]=cell2guide_for_adata.loc[cells_annotated_in_adata,guides_per_gene].sum(axis=1)
     
-    adata_here.obsm[pref+'.cell2guide']=cell2guide_for_adata
-    adata_here.obsm[pref+'.cell2gene']=cell2gene_for_adata
+    my_adata.obsm[pref+'.cell2guide']=cell2guide_for_adata
+    my_adata.obsm[pref+'.cell2gene']=cell2gene_for_adata
     if copy:
-        return(adata_here)
+        return(my_adata)
 
 
